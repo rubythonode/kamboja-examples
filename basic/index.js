541c57d3d43ef7ed29cf953f57ebbc6fdfad9039
Ketut Sandiarsa <ktutnik@gmail.com>
2017-02-15 19:06:28 +0800

    Remove included js files

--- basic/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var kamboja_express_1 = require("kamboja-express");
-var kamboja_1 = require("kamboja");
-require("reflect-metadata");
-var kamboja = new kamboja_1.Kamboja(new kamboja_express_1.ExpressEngine());
-kamboja.init().listen(5000);
